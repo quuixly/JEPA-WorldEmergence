1f2397638306dcb290573a6898021317c88c7a43
Kamil quuixly <quuixly@gmail.com>
2026-03-21 14:04:25 +0100

    feat: Add possibility to pass the file path for dataset file

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -3,14 +3,14 @@ import torch
 
 
 class OthelloDataset(Dataset):
-    def __init__(self, train = True):
+    def __init__(self, train = True, path = None):
         self.PADDING_TOKEN = 0
         self.train = train
 
         if train:
-            self.data = torch.load("dataset/train_dataset.pt")
+            self.data = torch.load(path if path else "./dataset/train_dataset.pt")
         else:
-            self.data = torch.load("dataset/test_dataset.pt")
+            self.data = torch.load(path if path else "./dataset/test_dataset.pt")
 
         self.target = torch.full_like(self.data, self.PADDING_TOKEN)
         self.target[:, :-1] = self.data[:, 1:]
